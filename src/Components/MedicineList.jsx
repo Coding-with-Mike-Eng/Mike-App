f38b42ad5f31ccb3ed0bfb9ab63a269710b999b2
Mike Apollo <91861360+Coding-with-Mike-Eng@users.noreply.github.com>
2025-07-27 12:19:25 +0300

    Update MedicineList.jsx

--- src/Components/MedicineList.jsx
+++ src/Components/MedicineList.jsx
@@ -4,26 +4,25 @@ function MedicineList({ medicines }) {
             <h2>Medicine List</h2>
             <table border="1" cellPadding="10">
                 <thead>
-                <tr>
-                    <th>Supplier Name</th>
-                    <th>Product Name</th>
-                    <th>Quantity</th>
-                    <th>Price (Ksh)</th>
-                </tr>
+                    <tr>
+                        <th>Manufacturer</th> {/* changed */}
+                        <th>Product Name</th>
+                        <th>Stock</th> {/* changed */}
+                        <th>Price (Ksh)</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {medicines.map((med, index) => (
-                    <tr key={index}>
-                        <td>{med.supplierName}</td>
-                        <td>{med.name}</td>
-                        <td>{med.quantity}</td>
-                        <td>{med.price}</td>
-                    </tr>
-                ))}
+                    {medicines.map((med, index) => (
+                        <tr key={index}>
+                            <td>{med.manufacturer}</td> {/* changed */}
+                            <td>{med.name}</td>
+                            <td>{med.stock}</td> {/* changed */}
+                            <td>{med.price}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
 }
-
 export default MedicineList;
